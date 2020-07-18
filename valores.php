39a6b72ce859c0add6dc7e8899bb2efd33de18e7
Saira Martinez Diaz <al221910498@gmail.com>
2020-07-18 15:35:25 -0500

    cambio

---
+++ valores.php
@@ -0,0 +1,47 @@
+<?php
+require_once './conexion.php';
+?>
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+<?php readfile('./menu.html'); ?>
+    <div class="container mt-3">
+        <div class="card">
+            <div class="card-header">
+                <a href="menu.html" class="btn btn-light btn-sm float-right"><i class="fa fa-reply"></i> regresar</a>
+                <br> <img src="img/valores.gif"width="350" height="350" alt="img_conference" class="rounded float-left mr-4">
+          <body>
+            <table>
+                <tr>
+                    <td>
+                    </p><h4><li type="circle">Confianza.</li></h4>
+                    <h4><li type="circle">Respeto.</li></h4>
+                    <h4><li type="circle">Humildad.</li></h4>
+                    <h4><li type="circle">Integridad.</li></h4>
+                    <h4><li type="circle">Lealtad.</li></h4>
+                    </td>
+                    
+                    <td>
+                     <h4><li type="circle">Responsabilidad.</li></h4>
+                    <h4><li type="circle">Lealtad.</li></h4>
+                    <h4><li type="circle">Integridad.</li></h4>
+                    <h4><li type="circle">Responsabilidad.</li></h4>
+                    
+                    </td>
+                </tr>
+            </table>
+    </form>
+</div>
+</div>
+</div>
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
