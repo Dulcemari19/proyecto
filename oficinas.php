102d3348fe7518c287073a2e03d6cc1b77db1e1b
Saira Martinez Diaz <al221910498@gmail.com>
2020-07-19 15:27:55 -0500

    Inmuebles cambio

---
+++ oficinas.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <?php
+    readfile('./menu.html');
+    ?>
+    <a href="inmuebles.php" class="btn btn-light btn-sm float-right"><i class="fa fa-reply"></i> regresar</a>
+    <img src="img/sdihouse.png"width="250" alt="img_conference" class="rounded float-left mr-4"></td><div class="jumbotron text-center">
+     <h1>SDI House</h1>
+      <p>¿Por cuánto tiempo me quedo con una propiedad? Mi preferencia: para siempre</p> 
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3></h3><br>
+        </br>
+         <img src="img/oficina1.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>
+         <a href="oficina1.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Información</button></a>
+        </div>
+        <div class="col-sm-4">
+          <h3></h3><br>
+        </br>
+          <img src="img/oficina2.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>
+          <a href="ofinica1.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Información</button></a>        </div>
+        <div class="col-sm-4">
+          <h3></h3> <br>
+        </br> 
+          <img src="img/oficina3.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>  
+          <a href="ofinica1.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Información</button></a>        </div>
+      </div>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
