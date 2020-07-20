d759c86a0e53e9e71cbf40a98d326847281a08f3
Saira Martinez Diaz <al221910498@gmail.com>
2020-07-20 15:14:02 -0500

    informacion de cada inmueble

---
+++ aguascalientescasa3.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <?php
+    readfile('./menu.html');
+    ?>
+    <a href="aguascalientesc.php" class="btn btn-light btn-sm float-right"><i class="fa fa-reply"></i> regresar</a>
+    <img src="img/sdihouse.png"width="250" alt="img_conference" class="rounded float-left mr-4"></td><div class="jumbotron text-center">
+     <h1>SDI House</h1>
+      <p>¿Por cuánto tiempo me quedo con una propiedad? Mi preferencia: para siempre</p> 
+    </div>
+    <main role="main" class="container">
+  <div class="jumbotron">
+    <h1>Aguascalientes</h1>
+    <body class="w3-content w3-border-left w3-border-right" ></body>
+    <center><b><strong><h2 class="w3-text-black">Residencial con Roof Garden</h2></strong></b></center>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="" data-slide-to="0" class="active"></li>
+        <li data-target="" data-slide-to="1"></li>
+        <li data-target="" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="img/casa3.jpg" alt="First slide">
+      </div>
+    </div>
+      <h4><strong>Servicios</strong></h4>
+              <div class="w3-row w3-large">
+                <div class="w3-col s6">
+                  <p><i class="fa fa-fw fa-water"></i> Agua Potable</p>
+                  <p><i class="fa fa-fw fa-rss"></i> Internet </p>
+                  <p><i class="fa fa-fw fa-phone"></i> Telefonia </p>
+                  <p><i class="fa fa-fw fa-broadcast-tower"></i> Energia Electrica</p>
+                  <p><i class="fa fa-fw fa-bezier-curve"></i> Gas </p>
+                  <p><i class="fa fa-fw fa-desktop"></i> Acceso a TV de paga </p>
+                </div>
+              </div>
+    <hr>
+    <h4><strong>Detalles</strong></h4>
+                  <div class="w3-row w3-large">
+                    <div class="w3-col s6">
+                      <p><i class="fa fa-fw fa-bed"></i> 5 Recamaras con closet</p>
+                      <p><i class="fa fa-fw fa-table"></i> Estudio</p>
+                      <p><i class="fa fa-fw fa-toilet"></i> 4 baños</p>
+                      <p><i class="fa fa-fw fa-warehouse"></i> Cochera </p>
+                      <p><i class="fa fa-fw fa-user-cog"></i> Construido (año) 2018</p>
+                      <p><i class="fa fa-fw fa-compress"></i> Construidos (m²) 250 </p>
+                      <p><i class="fa fa-fw fa-arrows-alt"></i> Terreno (m²) 240</p>
+                    </div>
+                 </div>
+    <hr>
+    
+    <h4><strong>Descripción</strong></h4>
+    <p>Preventa de Residencia Contemporánea en la zona de mayor Plusvalia de Aguascalientes 
+    al Nor-Poniente. Un proyecto que ha sido minuciosamente diseñado por la famosa firma arquitectonica 
+    de ARQDUO para disfrutar y vivir en armonía, amplios espacios, excelente iluminación, acabados y texturas 
+    que crean la diferencia entre una casa y un hogar. Disfruta de hermosos atardeceres con vista al imponente 
+    cerro del muerto, desde encantador Roof Garden, vive la tranquilidad de tener tu hogar dentro de uno de los 
+    desarrollos más exclusivos PUNTA DORADA, con increibles amenidades, acceso controlado y seguridad. Distribución 
+    Ubicada en calle Colindante a barda perimetral sin vecino enfrente. PB PA 3 Amplias recámaras, cada una con su 
+    baño propio, la principál cuenta con Walking Closeth , doble altura, sala de TV , vestibulo.</p>
+    <hr>
+    <h4><strong>Calcula tu Hipoteca</strong></h4>
+    <p>$ 4,420,000 Casa en Preventa</p>
+    <hr>
+  <!-- Contact -->
+  <div class="w3-container" id="contact">
+    <h4><strong>Contacto</strong></h4>
+    <i class="fa fa-map-marker" style="width:30px"></i>Ciudad de Mexico<br>
+    <i class="fa fa-phone" style="width:30px"></i>Telefono 55 7234679810<br>
+    <i class="fa fa-envelope" style="width:30px"> </i>Correo SDI12@gmail.com<br>
+  
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-border" type="text" placeholder="Nombre" required name="Nombre"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Correo" required name="Correo"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Mensaje" required name="Mensaje"></p>
+    <button type="submit" class="w3-button w3-black w3-third">Enviar un mensaje</button>
+    </form>
+  </div>
+  <hr>
+</main>
+                              <p><i class="fa fa-fw fa-mug-hot"></i> Cocina </p>
+                              <p><i class="fa fa-fw fa-couch"></i> Sala </p>
+                              <p><i class="fa fa-fw fa-mortar-pestle"></i> Comedor </p>
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+    </html>
