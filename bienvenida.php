c159d276c735f4a93224bffbe137c9bc02f6f1d9
Saira Martinez Diaz <al221910498@gmail.com>
2020-07-24 00:44:01 -0500

    Bienvenida

---
+++ bienvenida.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienveni@</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <?php readfile('./menu.html'); ?>
+    <div class="container mt-3">
+        <div class="card">
+            <div class="card-header">
+                <i class="fa fa-glass-cheers"></i> Bienvenid@
+            </div>
+            <div class="card-body">
+                Elige una opcion del menú
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
