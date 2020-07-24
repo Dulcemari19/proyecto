46a6b4682c4abab79ef3d3906c6fd5f52eb1ecc2
Saira Martinez Diaz <al221910498@gmail.com>
2020-07-24 02:24:47 -0500

    Cambios en bienvenida

--- bienvenida.php
+++ bienvenida.php
@@ -9,16 +9,31 @@
 </head>
 <body>
     <?php readfile('./menu.html'); ?>
-    <div class="container mt-3">
-        <div class="card">
-            <div class="card-header">
-                <i class="fa fa-glass-cheers"></i> Bienvenid@
-            </div>
-            <div class="card-body">
-                Elige una opcion del menú
-            </div>
-        </div>
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class="display-4">Bienvenido</h1><i class="check-double"></i>
+  <p class="lead">"Alguna gente busca un hermoso lugar; otros hacen de un lugar, algo hermoso."</p>
+</div>
+<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="10000">
+      <img src="img/casa1.jpg" class="d-block w-100" alt="100">
     </div>
+    <div class="carousel-item" data-interval="2000">
+      <img src="img/casa4.jpg" class="d-block w-100" alt="100">
+    </div>
+    <div class="carousel-item">
+      <img src="img/casa5.jpg" class="d-block w-100" alt="100">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
