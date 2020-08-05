f35a9878906943c3519c5d2576d818ab0836ed82
Valencia_Nava Itzel <al221910521@gmail.com>
2020-08-05 18:32:39 -0500

    zacatecaspromocion.php

---
+++ zacatecaspromocion1.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <?php
+    readfile('./menu.html');
+    ?>
+    <a href="zacatecasp.php" class="btn btn-light btn-sm float-right"><i class="fa fa-reply"></i> regresar</a>
+    <img src="img/sdihouse.png"width="250" alt="img_conference" class="rounded float-left mr-4"></td><div class="jumbotron text-center">
+     <h1>SDI House</h1>
+      <p>¿Por cuánto tiempo me quedo con una propiedad? Mi preferencia: para siempre</p> 
+    </div>
+    <main role="main" class="container">
+  <div class="jumbotron">
+    <h1>Zacatecas</h1>
+    <body class="w3-content w3-border-left w3-border-right" ></body>
+    <center><b><strong><h2 class="w3-text-black">Encinos</h2></strong></b></center>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="" data-slide-to="0" class="active"></li>
+        <li data-target="" data-slide-to="1"></li>
+        <li data-target="" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="img/p1zac.jpg" alt="First slide">
+    </div>
+    </div>
+      <h4><strong>Servicios</strong></h4>
+              <div class="w3-row w3-large">
+                <div class="w3-col s6">
+                  <p><i class="fa fa-fw fa-water"></i> Agua Potable</p>
+                  <p><i class="fa fa-fw fa-rss"></i> Internet </p>
+                  <p><i class="fa fa-fw fa-phone"></i> Telefonia </p>
+                  <p><i class="fa fa-fw fa-broadcast-tower"></i> Energia Electrica</p>
+                  <p><i class="fa fa-fw fa-bezier-curve"></i> Gas </p>
+                </div>
+              </div>
+    <hr>
+    <h4><strong>Detalles</strong></h4>
+                  <div class="w3-row w3-large">
+                    <div class="w3-col s6">
+                      <p><i class="fa fa-fw fa-bed"></i> 4 Recámaras</p>
+                      <p><i class="fa fa-fw fa-door-open"></i> 6 Habitaciones(en total)</p>
+                      <p><i class="fa fa-fw fa-toilet"></i> 2 baños</p>
+                      <p><i class="fa fa-fw fa-warehouse"></i> Almacen </p>
+                      <p><i class="fa fa-fw fa-user-cog"></i> Construido (año) 2019</p>
+                      <p><i class="fa fa-fw fa-compress"></i> Construidos (m²) 764 </p>
+                      <p><i class="fa fa-fw fa-arrows-alt"></i> Terreno (m²) 649</p>
+                    </div>
+                 </div>
+    <hr>
+    
+    <h4><strong>Descripción</strong></h4>
+    <p>Casa en venta Encinos. Cuenta con 2 niveles, en la planta de abajo cuenta concuenta  con 2 recamaras,
+     1 baño, cuenta con 1 cocina, una sala, 1 comedor y un almacen, en la segunda planta que es la de arriba
+     cuenta con 2 recamaras y un baño. Por la parte de afuera cuenta con un estacionamiento, y un pequeño
+    Patio todo para su gran comodidad.</p>
+    <hr>
+    <h4><strong>Calcula tu Hipoteca</strong></h4>
+    <p>$ 57,240  Condominio en Venta</p>
+    <hr>
+  <!-- Contact -->
+  <div class="w3-container" id="contact">
+    <h4><strong>Contacto</strong></h4>
+    <i class="fa fa-map-marker" style="width:30px"></i>Ciudad de Mexico<br>
+    <i class="fa fa-phone" style="width:30px"></i>Telefono 55 7234679810<br>
+    <i class="fa fa-envelope" style="width:30px"> </i>Correo SDI12@gmail.com<br>
+  
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-border" type="text" placeholder="Nombre" required name="Nombre"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Correo" required name="Correo"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Mensaje" required name="Mensaje"></p>
+    <button type="submit" class="w3-button w3-black w3-third">Enviar un mensaje</button>
+    </form>
+  </div>
+  <hr>
+  
+</main>
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+    </html>
