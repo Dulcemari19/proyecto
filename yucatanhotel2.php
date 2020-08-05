7252bbd014c320edb4baa9aaa214aa4e1be64b45
Valencia_Nava Itzel <al221910521@gmail.com>
2020-08-04 20:30:35 -0500

    yucatanhotel.php

---
+++ yucatanhotel2.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+    <?php
+    readfile('./menu.html');
+    ?>
+    <a href="yucatanh.php" class="btn btn-light btn-sm float-right"><i class="fa fa-reply"></i> regresar</a>
+    <img src="img/sdihouse.png"width="250" alt="img_conference" class="rounded float-left mr-4"></td><div class="jumbotron text-center">
+     <h1>SDI House</h1>
+      <p>¿Por cuánto tiempo me quedo con una propiedad? Mi preferencia: para siempre</p> 
+    </div>
+    <main role="main" class="container">
+  <div class="jumbotron">
+    <h1>Yucatan</h1>
+    <body class="w3-content w3-border-left w3-border-right" ></body>
+    <center><b><strong><h2 class="w3-text-black">Calipso</h2></strong></b></center>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="" data-slide-to="0" class="active"></li>
+        <li data-target="" data-slide-to="1"></li>
+        <li data-target="" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="img/h2yuc.jpg" alt="First slide">
+        </div>
+    </div>
+      <h4><strong>Servicios</strong></h4>
+              <div class="w3-row w3-large">
+                <div class="w3-col s6">
+                  <p><i class="fa fa-fw fa-water"></i> Agua Potable</p>
+                  <p><i class="fa fa-fw fa-broadcast-tower"></i> Energia Electrica</p>
+                  <p><i class="fa fa-fw fa-bezier-curve"></i> Gas </p>
+                </div>
+              </div>
+    <hr>
+    <h4><strong>Detalles</strong></h4>
+                  <div class="w3-row w3-large">
+                    <div class="w3-col s6">
+                      <p><i class="fa fa-fw fa-bed"></i> 1 Recámara</p>
+                      <p><i class="fa fa-fw fa-door-open"></i> 2 Habitaciones(en total)</p>
+                      <p><i class="fa fa-fw fa-toilet"></i> 1 Baños</p>
+                      <p><i class="fa fa-fw fa-warehouse"></i> 1 Estacionamiento </p>
+                      <p><i class="fa fa-fw fa-user-cog"></i> Construido (año) 2005</p>
+                      <p><i class="fa fa-fw fa-compress"></i> Construidos (m²) 311 </p>
+                      <p><i class="fa fa-fw fa-arrows-alt"></i> Nivel 3</p>
+                    </div>
+                 </div>
+    <hr>
+    
+    <h4><strong>Descripción</strong></h4>
+    <p>Hotel Calipso renta de departamento por dia, zona de Yucatan. Departamento con elevador
+    y cajones de estacionamiento, cuenta con una recamara por habitacion y un baño, asi como 
+    un ropero, una television, detro del hotel cuentan con una vista hacia la calle principal
+    Todo para su gran comodidad.</p>
+    <hr>
+    <h4><strong>Calcula tu Renta</strong></h4>
+    <p>$ 700</p>
+    <hr>
+  <!-- Contact -->
+  <div class="w3-container" id="contact">
+    <h4><strong>Contacto</strong></h4>
+    <i class="fa fa-map-marker" style="width:30px"></i>Ciudad de Mexico<br>
+    <i class="fa fa-phone" style="width:30px"></i>Telefono 55 7234679810<br>
+    <i class="fa fa-envelope" style="width:30px"> </i>Correo SDI12@gmail.com<br>
+  
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-border" type="text" placeholder="Nombre" required name="Nombre"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Correo" required name="Correo"></p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Mensaje" required name="Mensaje"></p>
+    <button type="submit" class="w3-button w3-black w3-third">Enviar un mensaje</button>
+    </form>
+  </div>
+  <hr>
+  
+</main>
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+    </html>
