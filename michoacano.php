7300495d8009fa0f43f6037a558bd54538b8be90
Dulce Maria Sevilla Ortiz <al221910491@gmail.com>
2020-08-13 13:27:28 -0500

    jalisco y michoacan

---
+++ michoacano.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">SDI</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Inicio
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="mision.php"><i class="fa fa-feather-alt"></i> Misión</a>
+                      <a class="dropdown-item" href="vision.php"><i class="fa fa-eye"></i> Visión</a>
+                      <a class="dropdown-item" href="valores.php"><i class="fa fa-people-carry"></i> Valores</a>
+                      <a class="dropdown-item" href="objetivos.php"><i class="fa fa-clipboard-list"></i> Obejtivos</a>
+                      <a class="dropdown-item" href="quienes_somos.php"><i class="fa fa-award"></i> ¿Quienes Somos?</a>
+                      <a class="dropdown-item" href="clientes.php"><i class="fa fa-users"></i>Clientes</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Inmuebles
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="casas.php"><i class="fa fa-house-damage"></i> Casas</a>
+                      <a class="dropdown-item" href="hoteles.php"><i class="fa fa-building"></i> Hoteles</a>
+                      <a class="dropdown-item" href="oficinas.php"><i class="fa fa-biohazard"></i> Oficinas</a>
+                      <a class="dropdown-item" href="terrenos.php"><i class="fa fa-vector-square"></i> Terrenos</a>
+                </li> 
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                    <a class="nav-link" href="terminar_sesion.php"><i class="fa fa-sign-out-alt"></i>Terminar sesión</a>
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                <button type="button" class="btn btn-dark">Buscar</button>
+            </form>
+        </div>
+    </nav>    <a href="oficinas.php" class="btn btn-light btn-sm float-right"><i class="fa fa-reply"></i> regresar</a>
+    <img src="img/sdihouse.png"width="250" alt="img_conference" class="rounded float-left mr-4"><div class="jumbotron text-center">
+     <h1>SDI House</h1>
+      <p>¿Por cuánto tiempo me quedo con una propiedad? Mi preferencia: para siempre</p> 
+    </div>
+    <div class="container">
+  <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal"></h4>
+      </div>
+      <div class="card-body">
+      <img src="img/Oficinas/Michoacán/Oficina 1/1.jpg"width="300" alt="img_conference" class="rounded float-left mr-4">
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>"El compromiso </li>
+          <li>es Contigo"</li>
+          <li>La mejor calidad aqui en SDI</li>
+        </ul>
+        <a href="michoacano1.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Observar</button></a>
+      </div>
+      </div>
+      <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal"></h4>
+      </div>
+      <div class="card-body">
+      <img src="img/Oficinas/Michoacán/Oficina 2/1.webp"width="300" alt="img_conference" class="rounded float-left mr-4">
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>"El compromiso </li>
+          <li>es Contigo"</li>
+          <li>La mejor calidad aqui en SDI</li>
+        </ul>
+        <a href="michoacano2.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Observar</button></a>
+      </div>
+      </div>
+      <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal"></h4>
+      </div>
+      <div class="card-body">
+      <img src="img/Oficinas/Michoacán/Oficina 3/1.webp"width="300" alt="img_conference" class="rounded float-left mr-4"> 
+      <ul class="list-unstyled mt-3 mb-4">
+          <li>"El compromiso </li>
+          <li>es Contigo"</li>
+          <li>La mejor calidad aqui en SDI</li>
+        </ul>
+        <a href="michoacano3.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Observar</button></a>
+      </div>
+    </div>
