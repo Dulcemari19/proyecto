24ef8e13a0a7faa5b5487b457b5ab9f3e6820f55
Saira Martinez Diaz <al221910498@gmail.com>
2020-08-13 15:10:57 -0500

    Cambio catálogo

--- catalogo.php
+++ catalogo.php
@@ -1,5 +1,4 @@
 <?php
-require_once './conexion.php';
 require_once './checa_sesion.php'
 ?>
 <!DOCTYPE html>
@@ -7,7 +6,7 @@ require_once './checa_sesion.php'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>Bienes Raices</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
 </head>
@@ -15,41 +14,34 @@ require_once './checa_sesion.php'
     <?php
     readfile('./menu.html');
     ?>
-    <div class="container mt-3">
-        <div class="card">
-           
-                        <?php
-                        $sql = 'select estados.idestado, municipios.idmunicipio from estados
-                        inner join estados on estados.iestados=municpios.idestado';                       
-                        foreach ($conexion->query($sql) as $registro) {
-                        
-                            echo <<<fin
-                            <div class="container">
-                            <div class="card-deck mb-3 text-center">
-                              <div class="card mb-4 shadow-sm">
-                                <div class="card-header">
-                                  <h4 class="my-0 font-weight-normal">Terreno en venta </h4>
-                                </div>
-                                <div class="card-body">
-                                <img src="img/Terrenos/Baja california/Terreno 1/1.webp"width="300" alt="img_conference" class="rounded float-left mr-4">
-                                  <ul class="list-unstyled mt-3 mb-4">
-                                    <li>"El compromiso </li>
-                                    <li>es Contigo"</li>
-                                    <li>La mejor calidad aqui en SDI</li>
-                                  </ul>
-                                  <a href="bajat.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Observar</button></a>
-                                </div>
-                                </div>
-                                </div>
-                            
-fin;
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <img src="img/sdihouse.png"width="250" alt="img_conference" class="rounded float-left mr-4"></td><div class="jumbotron text-center">
+     <h1>SDI House</h1>
+      <p>¿Por cuánto tiempo me quedo con una propiedad? Mi preferencia: para siempre</p> 
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3>Casas</h3>
+         <img src="img/1.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>
+         <a href="casas.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Observar</button></a>
+        </div>
+        <div class="col-sm-4">
+          <h3>Hoteles</h3>
+          <img src="img/2.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>
+          <a href="hoteles.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Observar</button></a>
+        </div>
+        <div class="col-sm-4">
+          <h3>Oficinas</h3>  
+          <img src="img/3.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>  
+          <a href="oficinas.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Observar</button></a>
+        </div>
+      </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <h3>Terrenos Si Construcciones</h3>
+            <img src="img/4.jpg"width="200" alt="img_conference" class="rounded float-left mr-4"></td>
+            <a href="terrenos.php"><button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-arrow-alt-circle-left"></i> Observar</button></a>
+          </div>
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
